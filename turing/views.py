1c8e84bc5c3ee1efbfa7fd199f5b54db8e175335
Alejandra Alarcon <alalarco@dcc.uchile.cl>
2019-10-01 19:20:37 -0300

    Fixed logout button

--- turing/views.py
+++ turing/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 from .forms import IniciarSesionForm
 
@@ -16,6 +16,8 @@ def home(request):
             if user is not None:
                 login(request, user)
                 return HttpResponseRedirect('/landing_page/')
+            else:
+                return render_to_response('LandingPageNotLogged.html', {'invalid': True, 'form': form})
     else:
         form = IniciarSesionForm()
     return render(request, 'LandingPageNotLogged.html', {'form': form})
