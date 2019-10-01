9c3ae73257636a4b2a5a23845418c808426f2fe2
Alejandra Alarcon <alalarco@dcc.uchile.cl>
2019-10-01 18:43:59 -0300

    Password input is not in plain text anymore

--- turing/forms.py
+++ turing/forms.py
@@ -3,5 +3,8 @@ from .models import *
 
 
 class IniciarSesionForm(forms.Form):
-    username = forms.CharField(label='Nombre de usuario')
-    password = forms.CharField(label='Contraseña')
+    username = forms.CharField(label='Nombre de usuario', widget=forms.TextInput(\
+        attrs={'size': '15em'}))
+    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput(\
+        attrs={'size': '15em'}))
+
