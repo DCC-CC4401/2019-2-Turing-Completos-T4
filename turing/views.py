ab6d4eab5c7401a239e4aa7c4490da05bd8acf8b
Alejandra Alarcon <alalarco@dcc.uchile.cl>
2019-10-01 17:02:16 -0300

    Updated view for login auth

--- turing/views.py
+++ turing/views.py
@@ -1,13 +1,36 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from .forms import IniciarSesionForm
 
 
 def home(request):
-    return render(request, 'LandingPageNotLogged.html')
+    if request.method == 'POST':
+        form = IniciarSesionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                print("hola")
+                return HttpResponseRedirect('/landing_page/')
+    else:
+        form = IniciarSesionForm()
+    return render(request, 'LandingPageNotLogged.html', {'form': form})
 
 
+@login_required
 def landing_page(request):
     return render(request, 'LandingPage.html')
 
 
 def my_profile(request):
     return render(request, 'UserProfile.html')
+
+
+
+
