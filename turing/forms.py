969e1a7705f118ac82a83e6947a9a4f5bad647b3
lucas516 <44588020+lucas516@users.noreply.github.com>
2019-10-02 18:35:02 -0300

    Update forms.py

--- turing/forms.py
+++ turing/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 from .models import *
 
-
 class IniciarSesionForm(forms.Form):
-    username = forms.CharField(label='Nombre de usuario', widget=forms.TextInput(\
-        attrs={'size': '15em'}))
-    password = forms.CharField(label='Contraseña', widget=forms.PasswordInput(\
-        attrs={'size': '15em'}))
-
+    username = forms.CharField(label='', widget=forms.TextInput(\
+        attrs={'class': 'rounded mx-1','size': '20em', 'placeholder':"Correo" }))
+    password = forms.CharField(label='', widget=forms.PasswordInput(\
+        attrs={'class': 'rounded mx-1', 'size': '20em','placeholder':"Contraseña"}))
