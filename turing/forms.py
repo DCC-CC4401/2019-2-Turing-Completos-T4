8138bfa4326166b31218c58bb50f2581d3ff32e8
Fabiola RIvera <fabiolarivera@ug.uchile.cl>
2019-10-06 13:40:46 -0300

    Reformateo

--- turing/forms.py
+++ turing/forms.py
@@ -1,8 +1,8 @@
 from django import forms
-from .models import *
+
 
 class IniciarSesionForm(forms.Form):
-    username = forms.CharField(label='', widget=forms.TextInput(\
-        attrs={'class': 'rounded mx-1','size': '20em', 'placeholder':"Correo" }))
-    password = forms.CharField(label='', widget=forms.PasswordInput(\
-        attrs={'class': 'rounded mx-1', 'size': '20em','placeholder':"Contraseña"}))
+    username = forms.CharField(label='', widget=forms.TextInput( \
+        attrs={'class': 'rounded mx-1', 'size': '20em', 'placeholder': "Correo"}))
+    password = forms.CharField(label='', widget=forms.PasswordInput( \
+        attrs={'class': 'rounded mx-1', 'size': '20em', 'placeholder': "Contraseña"}))
